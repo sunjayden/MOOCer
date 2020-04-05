874980fda4703cbac1ebf7da43c32d1cff854fbf
Jayden <jayden_sun@outlook.com>
2020-04-05 00:35:48 -0400

    Fixed reference issue

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import "/Users/yulil/Desktop/MOOCer/node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "/Users/yulil/Desktop/MOOCer/src/client/app.css";
+import "./app.css";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import Main from "./components/app-main";
